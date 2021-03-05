d61706dc0db46d8b83fe391e611401d05bbae062
Christopher Hull <cpahull@btinternet.com>
2021-03-05 17:00:28 +0000

    Initial Commit

---
+++ SwiftUISourceList/SwiftUISourceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISourceListApp.swift
+//  SwiftUISourceList
+//
+//  Created by cpahull on 05/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISourceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
