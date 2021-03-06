ce1d6fc16f0f19973bb76225bf4cc92b4053623a
Christopher Hull <cpahull@btinternet.com>
2021-03-06 23:12:37 +0000

    Initial commit proving load of nodes from Bundle.main

--- SwiftUISourceList/ContentView.swift
+++ SwiftUISourceList/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+enum NodeType: Int, Codable {
+    case tree
+    case branch
+    case leaf
+}
+
+class Node: NSObject, Identifiable, Codable {
+    var id = UUID()
+    var type: NodeType
+    @objc var name: String
+    @objc var cleared: Bool
+    @objc var children = [Node]()
+    @objc var childrenCount: Int {return children.count}
+    @objc var isLeaf: Bool {return children.count == 0}
+
+    init(type: NodeType, name: String, cleared: Bool, children: [Node]) {
+        self.type = type
+        self.name = name
+        self.cleared = cleared
+        self.children = children
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var nodes = [Node]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Load") {
+            nodes = Bundle.main.decode([Node].self, from: "nodes.json")
+            for i in 0 ..< nodes.count {
+                print(nodes[i].name)
+                for j in 0 ..< nodes[i].children.count {
+                    print(nodes[i].children[j].name)
+                    for k in 0 ..< nodes[i].children[j].children.count {
+                        print(nodes[i].children[j].children[k].name)
+                    }
+                }
+            }
+        }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
--- SwiftUISourceList/SwiftUISourceListApp.swift
+++ SwiftUISourceList/SwiftUISourceListApp.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct SwiftUISourceListApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
 }
